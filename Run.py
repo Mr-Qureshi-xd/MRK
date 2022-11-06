1968735a8f2addb1cb66e1f7866861242ac2b96a
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2022-11-06 20:31:26 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,2 +1,17 @@
-import mr
-mr.main()
+import os, platform, time
+print('Checking Updates...')
+time.sleep(2)
+os.system('https://www.youtube.com/c/MrQureshiTech')
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Mrk
+    Mrk.riaz()
+elif bit == '32bit':
+    import Mrk32
+    Mrk32.riaz()
